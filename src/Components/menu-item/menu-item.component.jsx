add3cb13ff89d0248bff0435a9f1a5953d705636
majd maihoub <majd.maihoub@meta-itech.com>
2022-01-31 14:24:59 +0200

    setting up ecommerce

---
+++ src/Components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './menu-item.styles.scss';
+const MenuItem = ({ title, imageUrl }) => (
+  <div style={{ backgroundImage: `url(${imageUrl}) ` }} className='menu-item'>
+    <div className='content'>
+      <h1 className='title'>{title}</h1>
+      <span className='sub-title'>Shop Now</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
