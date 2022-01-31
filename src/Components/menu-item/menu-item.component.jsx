1f9a5bbc40f07451150575ec10754a9e31dbc6eb
majd maihoub <majd.maihoub@meta-itech.com>
2022-01-31 20:17:21 +0200

    Transition styling menu item

--- src/Components/menu-item/menu-item.component.jsx
+++ src/Components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './menu-item.styles.scss';
-const MenuItem = ({ title, imageUrl }) => (
-  <div style={{ backgroundImage: `url(${imageUrl}) ` }} className='menu-item'>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{ backgroundImage: `url(${imageUrl}) ` }}
+    />
     <div className='content'>
-      <h1 className='title'>{title}</h1>
+      <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='sub-title'>Shop Now</span>
     </div>
   </div>
