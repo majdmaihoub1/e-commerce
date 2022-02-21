ee7013ec230157bf6e4bc086b5da490146d91e05
majd maihoub <majd.maihoub@meta-itech.com>
2022-02-21 22:56:21 +0200

    update-menu-item-with-react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import HomePage from './Pages/homepage/hompage.component';
+import { Switch, Route } from 'react-router-dom';
 
+const HatsPage = () => (
+  <div>
+    <h1>hats Page</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
